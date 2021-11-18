1accf516540e934313d1437a54cf1c3c0b96daac
Kiara Lazarova <kival1020@yahoo.com>
2021-11-18 19:34:06 +0200

    Added home page template and styling and refactored HomePageController.java to handle that functionality

--- src/main/java/course/springadvanced/cookeasy/web/HomeController.java
+++ src/main/java/course/springadvanced/cookeasy/web/HomeController.java
@@ -1,12 +1,16 @@
 package course.springadvanced.cookeasy.web;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping(value = "/")
-    public String retrieveHomePage() {
-        return "index";
+    public String retrieveHomePage(@AuthenticationPrincipal UserDetails user) {
+        if(user == null) return "index";
+
+        return "home";
     }
 }
